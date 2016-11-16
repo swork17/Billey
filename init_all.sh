2b4279cd87d0da25a3342f9ccb6e5f882c431329
Kevin Boucher <kevin.b1706@gmail.com>
2016-11-16 17:28:16 +0100

    ShellScript

---
+++ init_all.sh
@@ -0,0 +1,5 @@
+xterm -title "Server" -hold -e "./bin/Server server1 4242" 
+xterm -title "Client 1" -hold -e "./bin/Client 1" 
+xterm -title "Client 2" -hold -e "./bin/Client 2" 
+xterm -title "Client 3" -hold -e "./bin/Client 3" 
+xterm -title "Client 4" -hold -e "./bin/Client 4" 
