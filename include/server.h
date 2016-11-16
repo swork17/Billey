46c8a34e69c43b826cef10faecdf401fa130c565
Kevin Boucher <kevin.b1706@gmail.com>
2016-11-16 16:33:11 +0100

    Ajout ecoute serveur multi clients(fork)

--- include/server.h
+++ include/server.h
@@ -2,5 +2,6 @@
 # define SERVER_H
 
 int init_server(char *adress, char *port);
+void error(const char *msg);
 
 #endif /* !SERVER_H */
--- src/server/server.c
+++ src/server/server.c
@@ -1,11 +1,67 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include "../../include/server.h"
 
+#define MAX_CLIENT 4
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
 int init_server(char *adress, char *port) {
+	int sockfd, newsockfd, _numport;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
 
-	int _port = atoi(port);
-	printf("Adress : %s\nPort : %d\n\n", adress, _port);
+    _numport = atoi(port);
+	printf("Adress : %s\nPort : %d\n\n", adress, _numport);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+         return -1;
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(_numport);
+     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("LISTEN ERROR");
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
 
-	return 0;
+   int pid;
+   int client = 0;
+    while (1) {
+         newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+         client += 1; 
+         if (newsockfd < 0)
+                error("SOCK ERROR");
+         pid = fork();
+         if (pid < 0) 
+              error("PID ERROR");
+         if (pid == 0) {
+            close(sockfd);
+            bzero(buffer, 256);
+            n = read(newsockfd, buffer, 255);
+            if (n < 0)
+                error("READ ERROR");
+            printf("Client %d :(%d) %s\n", client, pid, buffer);
+            n = write(newsockfd, "I got your message", 18);
+            if (n < 0)
+                 error("WRITE ERROR");
+            close(newsockfd);
+          }
+          else
+             close(newsockfd);
+    }
+
+    return 0;
 }
