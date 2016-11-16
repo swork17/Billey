70053a414674ce8de4f2bae2defd4281441c4cef
Lanxss <fabien226@hotmail.fr>
2016-11-16 13:48:34 +0100

    Création du client.h, client.c client.c : Vérfication et conversion du numéro de fichier pour le code du guichet

--- src/client/main.c
+++ src/client/main.c
@@ -1,11 +1,12 @@
 // Includes
 #include <stdio.h>
 #include <stdlib.h>
+#include "../../include/client.h"
 
 int main(int argc, char **argv){
 	
-	int i_idclient = 0; 
-
+	int i_idguichet = 0; 
+	char c_guichet;
 
 	// Nombre d'argument
 	if(argc != 2)
@@ -14,27 +15,18 @@ int main(int argc, char **argv){
 		return -1;
 	}
 
+	// Récupération du numéro, ex : Client 1
+	i_idguichet = atoi(argv[1]);
 
-	i_idclient = atoi(argv[1]);
+	// Vérifie et converti le numéro du guichet par son code, ex : 1 = 'A'
+	c_guichet = convert_type_guichet(i_idguichet);
 
-	if(i_idclient == 0 || i_idclient > 4)
+	if(c_guichet == ' ')
 	{
 		printf("Erreur dans le fichier client.c, argument non valide !\n");
 		return -1;
 	}
-
-	printf("Bienvenu au guichet n°%i\n", i_idclient);
-
-	/*
-	char c_zone[1] = {0};
 	
-	printf("Dans quelle zone souhaitez-vous être situé ? ");
+	init_client(c_guichet);
+} 
 
-  scanf("%s", c_zone);
-
-  printf("Vous avez choisi la zone : %s\n", c_zone);
-
-
-	*/
-
-} // Fin main
