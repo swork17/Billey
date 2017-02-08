29612dcde3c1dc755e3527956a05cc39d0b96545
Lanxss <fabien226@hotmail.fr>
2017-02-08 10:50:01 +0100

    Passage du port en parametre du init_all.sh

--- init_all.sh
+++ init_all.sh
@@ -1,6 +1,7 @@
+PORT="$1"
 make fclean
 make
-xterm -title "Client 1" -hold -e "./bin/Client 8888 1" &
-xterm -title "Client 2" -hold -e "./bin/Client 8888 2" &
-xterm -title "Client 3" -hold -e "./bin/Client 8888 3" &
-xterm -title "Client 4" -hold -e "./bin/Client 8888 4" &
+xterm -title "Client 1" -hold -e "./bin/Client "$PORT" 1" &
+xterm -title "Client 2" -hold -e "./bin/Client "$PORT" 2" &
+xterm -title "Client 3" -hold -e "./bin/Client "$PORT" 3" &
+xterm -title "Client 4" -hold -e "./bin/Client "$PORT" 4" &
